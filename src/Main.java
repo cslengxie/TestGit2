06d7ded52a8a0b183c30d43a44bb155c3c554a70
cslengxie <cslengxie@163.com>
2018-11-25 16:10:29 +0800

    没事闲的

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        String str = null;
-        System.out.println(str.isEmpty());
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        Object[] strs = list.toArray();
+        for (Object o:strs
+             ) {
+            System.out.println(o);
+        }
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(new Date());
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
